74ce49b623c96c28ca93d273d1e32c77b7ceb734
AgileDataScientist <lhallman4@gmail.com>
2022-02-25 14:25:09 -0500

    Multiclipboard first commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tech With Tim Applications
+
+## Appendix
+
+### File Structure
+_python_learning-applications
+    - base
+    - tech_with_tim
+      - multiclipboard
+        - multiclipboard.py
+        - README.md 
---
+++ tech_with_tim/multiclipboard/multiclipboard.py
@@ -0,0 +1,18 @@
+# YouTube: Tech With Tim - 3 Automation Projects for Beginners (https://youtu.be/Oz3W-LKfafE)
+
+# Import Python standard modules
+import sys
+import json
+from base import clipboard
+
+# Prints the arguments submitted with command
+print(sys.argv)
+
+if command.lowercase() == "save":
+    print(command)
+elif command.lowercase() == "load":
+    print(command)
+elif command.lowercase() == "list":
+    print(command)
+else:
+    print("Please enter a single command.")
